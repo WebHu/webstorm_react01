88c45571ac32cec4c62498fc05a993ce39f9b8a3
Gamehu <gamehu@yeah.net>
2016-12-20 18:13:53 +0800

    second reactDemo
    第二个reactDemo

---
+++ src/js/entry.js
@@ -0,0 +1,8 @@
+/**
+ * Created by elane on 2016/12/13.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './helloworld';
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
---
+++ src/js/helloworld.js
@@ -0,0 +1,17 @@
+/**
+ * Created by elane on 2016/12/13.
+ */
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+        <div>
+        <h1>Hello, world.</h1>
+        <p>this is a simple section</p>
+        <p>终于搭建成功了,我靠太他麼複雜了</p>
+        </div>
+
+    );
+    }
+}
